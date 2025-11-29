19978f9a5fd6662f439394a94b58e74d897d38bd
DarkSkillz <githubskillz@gmail.com>
2025-11-29 16:56:20 +0100

    Add function to get player choice

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ function getSystemChoice() {
     let randomNumber = Math.floor(Math.random() * choices.length);
     let systemChoice = choices[randomNumber];
     console.log("System's Choice: " + systemChoice); //! Debug Only
+    return systemChoice;
+}
+
+function getPlayerChoice() {
+    let promptedChoice = prompt("Choose one (Rock, Paper, Scissors)");
+    let playerChoice = promptedChoice.trim().toLowerCase();
+    if (!choices.includes(playerChoice)) {
+        getPlayerChoice();
+    }
+    else {
+        console.log(playerChoice); //! Debug Only
+        return playerChoice;
+    }
 }
 
 // The gameplay loop
 getSystemChoice();
+getPlayerChoice();
